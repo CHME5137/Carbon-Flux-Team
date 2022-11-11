aef5bd1c853820a7a6a9a2fdc325f357ac7142cc
katiehoyt <59654007+katiehoyt@users.noreply.github.com>
2022-11-11 10:34:28 -0500

    Adding mfapy files
    Adding mfapy files from Matsuda et al

---
+++ sample/Example_1_toymodel.py
@@ -0,0 +1,36 @@
+﻿#-------------------------------------------------------------------------------
+# Name:        mfapy Example_1_toymodel.py Example code of mfapy
+#-------------------------------------------------------------------------------
+import mfapy
+# Model construction
+reactions, reversible, metabolites, fragments\
+ = mfapy.mfapyio.load_metabolic_model("example_1_toymodel_model.txt")
+model = mfapy.metabolicmodel.MetabolicModel(reactions, reversible,\
+ metabolites, fragments)
+# Addition of constraints
+state = model.load_states("Example_1_toymodel_status.csv", format = 'csv')
+model.set_constraints_from_state_dict(state)
+model.update()
+# Generation of CarbonSource instance
+cs1 = model.generate_carbon_source_template()
+cs1.set_each_isotopomer('AcCoA', {'#10':0.5})
+cs2 = model.generate_carbon_source_template()
+cs2.set_each_isotopomer('AcCoA', {'#11':0.5})
+# Load MDV data
+mdv1 = model.load_mdv_data("Example_1_MDV1.txt")
+mdv2 = model.load_mdv_data("Example_1_MDV2.txt")
+# Flux estimation Step 1: Setting experimments
+model.set_experiment('ex1', mdv1, cs1)
+model.set_experiment('ex2', mdv2, cs2)
+# Flux estimation step 2: Generation of intical flux vectors
+endstate, flux = model.generate_initial_states(50, 4, method ="parallel")
+# Flux estimation step 3: Fitting model
+for method in ["GN_CRS2_LM", "LN_PRAXIS", "SLSQP"]:
+    endstate, RSS, flux = model.fitting_flux(method = method, flux = flux)
+model.show_results([("final", flux[0])]) # Show result
+# Estimation of 95% CI
+ci_edge = model.generate_ci_template(targets = [('reaction', "v3")])
+ci = model.search_ci(ci_edge, flux[0], method = 'grid')
+lb = ci['data'][('reaction', "v3")]['lower_boundary']
+ub = ci['data'][('reaction', "v3")]['upper_boundary']
+print("v3", "Lower bondary:",lb, "Upper boundary:", ub)
---
+++ setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Learn more: https://github.com/fumiomatsuda/mfapy
+#
+from setuptools import setup, find_packages
+import sys
+import os, os.path
+pyversion = sys.version_info[0]
+pkgdir = 'python%s' % pyversion
+
+def load_requires_from_file(fname):
+    if not os.path.exists(fname):
+        raise IOError(fname)
+    return [pkg.strip() for pkg in open(fname, 'r')]
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='mfapy',
+    version='0.6.1',
+    package_dir = {'mfapy': ""},
+    py_modules =  ["metabolicmodel", "carbonsource", "mfapyio", "mdv", "optimize"],
+    description = "mfapy: a python package for the 13C metabolic flux analysis",
+    platforms = ["Windows", "Linux", "Unix"],
+    long_description=readme,
+    author='Fumio Matsuda',
+    author_email='fmatsuda@osaka-u.ac.jp',
+    install_requires=load_requires_from_file('requirements.txt'),
+    url='https://github.com/fumiomatsuda/mfapy',
+    license=license,
+
+    test_suite='tests'
+)
+
